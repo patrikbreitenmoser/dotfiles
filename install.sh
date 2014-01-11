6c5be245f39d4228394f1f08fd5103dd31b327cb
Patrik Breitenmoser <info@patrikbreitenmoser.ch>
2014-01-11 23:58:53 +0100

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env zsh
+
+# Shell has to be zsh
+# use chsh -s /bin/zsh to change shell for the user
+# A simple script for setting up OSX dev environment.
+
+sudo -v # ask for password only at the beginning
+
+# dev="$HOME/dotfiles"
+homebin="$HOME/bin"
+
+# pushd .
+# mkdir -p $dev
+mkdir -p $homebin
+
+export PATH=~/bin:$PATH
+
+# cd $dev
+
+# echo 'Enter new hostname of the machine (e.g. todesstern)'
+#   read hostname
+#   echo "Setting new hostname to $hostname..."
+#   scutil --set HostName "$hostname"
+#   compname=$(sudo scutil --get HostName | tr '-' '.')
+#   echo "Setting computer name to $compname"
+#   scutil --set ComputerName "$compname"
+#   sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$compname"
+
+# echo 'Enter Git Username'
+#   read git_username
+#   echo "Setting Git Username to $git_username"
+#   git config --global user.name "$git_username"
+
+# echo 'Enter Git Email'
+#   read git_email
+#   echo "Setting Git Username to $git_email"
+#   git config --global user.email "$git_email"
+
+
+
+
+# pub=$HOME/.ssh/id_rsa.pub
+# echo 'Checking for SSH key, generating one if it does not exist...'
+#   [[ -f $pub ]] || ssh-keygen -t rsa
+
+# echo 'Copying public key to clipboard. Paste it into your Github account...'
+#   [[ -f $pub ]] && cat $pub | pbcopy
+#   open 'https://github.com/account/ssh'
+
+# If we on OS X, install homebrew and tweak system a bit.
+if [[ `uname` == 'Darwin' ]]; then
+  which -s brew
+  if [[ $? != 0 ]]; then
+    echo 'Installing Homebrew...'
+      ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+      brew doctor
+      brew update
+    echo 'Installing Cask'
+      brew install htop
+      brew tap phinze/homebrew-cask
+      brew install brew-cask
+      brew tap caskroom/versions
+  fi
+
+  # echo 'Tweaking OS X...'
+  #   source 'install/osx.sh'
+
+  echo 'Installing APPS - be patient'
+    source 'install/cask.sh'
+fi
+
+
+
+# echo 'Applying sublime config...'
+#   st=$(pwd)/sublime/packages
+#   as="$HOME/Application Support/Sublime Text 3/Packages"
+#   asprefs="$as/User/Preferences.sublime-settings"
+#   if [[ -d "$as" ]]; then
+#     for theme in $st/Theme*; do
+#       cp -r $theme $as
+#     done
+#     rm $asprefs
+#     cp -r $st/pm-themes $as
+#   else
+#     echo "Install Sublime Text http://www.sublimetext.com"
+#   fi
+
+
+
+# popd
+
---
+++ install/cask.sh
@@ -0,0 +1,43 @@
+echo 'Installing APPS'
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+# brew cask install suspicious-package quicklook-json qlmarkdown qlstephen qlcolorcode quicklook-csv
+
+# Install Sublime Text 3 and Plugins
+# brew cask install sublime-text3
+#ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+
+
+# Remove the "outdated" directories
+# http://www.alexconrad.org/2013/07/sync-sublime-text-3-settings-with.html
+# cd ~/Library/Application\ Support/Sublime\ Text\ 3
+# rm -rf Packages/
+# rm -rf Installed\ Packages/
+
+
+# # Then symlink your Dropbox directories back locally
+# $ ln -s ~/dotfiles/sublime-text-3/Packages/
+# $ ln -s ~/dotfiles/sublime-text-3/Installed\ Packages/
+
+# brew cask install alfred
+# brew cask install onepassword
+# brew cask install dropbox
+# brew cask install google-chrome
+# brew cask install evernote
+# brew cask install transmit
+# brew cask install sequel-pro
+# brew cask install virtualbox
+# brew cask install vagrant 
+# brew cask install kaleidoscope
+# brew cask install skype
+# brew cask install divvy
+# brew cask install daisydisk
+# brew cask install google-chrome-canary
+# brew cask install airmail-beta
+# brew cask install cleanmymac
+# brew cask install codebug
+# brew cask install hipchat
+# brew cask install spotify
+# brew cask install vlc
+# brew cask install twitterrific
+# brew cleanup

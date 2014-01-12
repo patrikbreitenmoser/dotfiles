903aa2e4135ba2448c0b84bc3bec86648570b2f5
Patrik Breitenmoser <info@patrikbreitenmoser.ch>
2014-01-12 17:09:30 +0100

    s

---
+++ install/homesick.sh
@@ -0,0 +1,8 @@
+#installing homesick https://github.com/technicalpickles/homesick
+gem install homesick
+
+#clone dotfiles repo
+homesick clone patrikbreitenmoser/dotfiles
+
+#symlink dotfiles
+homesick symlink dotfiles
